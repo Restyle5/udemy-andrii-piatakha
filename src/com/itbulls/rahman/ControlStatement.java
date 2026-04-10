0271227ef0f5a90829a647103a42069d4f9ed67e
restyle5 <abdurtinker@gmail.com>
2026-04-10 17:08:15 +0800

    WIP section_7

---
+++ src/com/itbulls/rahman/ControlStatement.java
@@ -0,0 +1,43 @@
+package com.itbulls.rahman;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class ControlStatement {
+
+	public static void main(String[] args) {
+
+		
+//		List<String> list = Arrays.asList(args);
+//		
+//		boolean isAdmin = list.contains("--admin");
+//		boolean isGuest = list.contains("--guest");
+//		
+//		
+//		if(isAdmin && isGuest)
+//		{
+//			System.out.println("Please, select either 'ADMIN' or 'GUEST' mode for this program");
+//		}else if(isAdmin) 
+//		{
+//			System.out.println("Hello, Admin!");
+//		}else if(isGuest)
+//		{
+//			System.out.println("Hello, Guest!");
+//		}
+		
+		
+//      // Second Exercise.
+//		if(args.length < 2 )
+//        {
+//            System.out.println("not enough arguments");
+//        }else{
+//            
+//            BigDecimal a = new BigDecimal(args[0]);
+//            BigDecimal b = new BigDecimal(args[1]);
+//            
+//            System.out.println(a.add(b));
+//        }
+
+	}
+}
