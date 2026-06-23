bd6163f0a2aad670a1d8a4af187074a8cd23ecef
restyle5 <abdurtinker@gmail.com>
2026-06-23 18:05:24 +0800

    Instant, LocalDateTime, Period, Duration

--- src/chapter/ChapterTwentyEight.java
+++ src/chapter/ChapterTwentyEight.java
@@ -2,6 +2,11 @@ package chapter;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
@@ -33,7 +38,9 @@ public class ChapterTwentyEight implements Chapter{
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-		topicCalendar();
+//		topicCalendar();
+		topicInstant();
+		topicLocalDateTime();
 		
 	}
 	
@@ -49,7 +56,7 @@ public class ChapterTwentyEight implements Chapter{
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss a");
 		System.out.println("SimpleDateFormat: "	+ sdf.format(calendar.getTime()));
 
-		// ======== Date instance =========
+		// ======== Date instance =========		
 		try {
 		Date d = new Date();
 		Date dateParsed = sdf.parse("10/01/1999 10:02:02 PM");
@@ -59,7 +66,35 @@ public class ChapterTwentyEight implements Chapter{
 		}catch(ParseException e) {
 			e.printStackTrace();	
 		}
+	}
+	
+	public static void topicInstant()
+	{
+		System.out.println("=== Instant class ===");
+		Instant instant1 = Instant.now();
+		System.out.println("Instant_now: " + instant1.toString());
 		
+		Instant instant2 = Instant.now(Clock.systemUTC());
+		
+		System.out.println("With Parameter: Clock.SystemUTC(): " + instant2);
+		System.out.println(instant1.plusNanos(12242424));
+		System.out.println(instant1.minusSeconds(120));
+		System.out.println(instant1.plus(2, ChronoUnit.DAYS));
+		System.out.println(instant1.minus(1, ChronoUnit.HOURS));
+		
+		
+		System.out.print(instant1.isBefore(instant2));
+		System.out.println("\n" + instant2.isBefore(instant1));
+		System.out.println(instant1.compareTo(instant2));
+	}
+	
+	public static void topicLocalDateTime()
+	{
+		LocalDateTime ldt = LocalDateTime.now();
+		
+		System.out.println("LocalDateTime: " + ldt.toString());
+		System.out.println(ldt.plusDays(4).toString());
+		System.out.println(ldt.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
 	}
 
 }
