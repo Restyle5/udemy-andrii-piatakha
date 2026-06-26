e72006c7d1f96de172b70920c38d3d110689555c
restyle5 <abdurtinker@gmail.com>
2026-06-26 17:44:38 +0800

    add Reactive Programmign (Flow API/Reactive Streams)

--- src/chapter/ChapterThirty.java
+++ src/chapter/ChapterThirty.java
@@ -3,13 +3,17 @@ package chapter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.SubmissionPublisher;
+import java.util.concurrent.TimeUnit;
+
+import com.itbulls.rahman.entity.MySubscriber;
 
 public class ChapterThirty implements Chapter{
 
 	private Note notes;
 	
 	public ChapterThirty() {
-		Note notes = new ChapterNote(Arrays.asList("String Builder & String Buffer", "Optional"));
+		Note notes = new ChapterNote(Arrays.asList("String Builder & String Buffer", "Optional", "Reactive Programming (FlowAPI, Reactive stream)"));
 		this.notes = notes;
 	}
 	
@@ -27,7 +31,8 @@ public class ChapterThirty implements Chapter{
 	public void execute() {
 		topicStringBuilderAndBuffer();
 		topicOptional();
-	}
+		topicReactiveProgramming();
+	}	
 	
 	public static void topicStringBuilderAndBuffer() {
 		
@@ -60,6 +65,33 @@ public class ChapterThirty implements Chapter{
 		
 		String filteredName = test.filter(name -> name.contains("ker")).orElse("Name not found ");
 		System.out.println(filteredName);
+	}
+	
+	public static void topicReactiveProgramming() {	
+	
+	try {
+	SubmissionPublisher<Integer> publisher 	= new SubmissionPublisher<>();
+		// Create a custom Processor
+		MySubscriber subscriber = new MySubscriber();
+
+        // Subscribe the Subscriber to the Publisher
+        publisher.subscribe(subscriber);
+        
+        TimeUnit.SECONDS.sleep(1);
+        // Publish some items to the Publisher
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Publisher: Emitting item " + i);
+            publisher.submit(i);
+        }
+        
+        // Close the publisher to signal completion
+        publisher.close();
+        
+     // Wait for subscriber to finish
+        TimeUnit.SECONDS.sleep(5);
+        
+	}catch(Exception e) { e.printStackTrace();}
 	}
 
 }
